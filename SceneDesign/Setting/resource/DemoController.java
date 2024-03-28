1494fae1a8f682d41eec535646fbb67a8b4dfab8
IamTryinghardnow <163639747+IamTryinghardnow@users.noreply.github.com>
2024-03-28 22:03:27 +0800

    Add files via upload

---
+++ SceneDesign/Setting/resource/DemoController.java
@@ -0,0 +1,26 @@
+package Setting.resource;
+
+//public class DemoController {
+//    @FXML               //引入id
+//    Label label;
+//
+//    @FXML
+//    Button button;
+//
+//    //设置事件
+//    public  void Onup(){
+//        label.setLayoutY(label.getLayoutY()-5);
+//    }
+//    //initialize方法，组件绑定之后自动调用，可以用启动时加载组件数据
+//    public void initialize(){
+//
+//    }
+//}
+//public class DemoController {
+//    @FXML
+//    private Circle ci;
+//    public void circleLocationBind(Scene scene){
+//        ci.centerXProperty().bind(scene.widthProperty().divide(2));
+//        ci.centerYProperty().bind(scene.heightProperty().divide(2));
+//    }
+//}
